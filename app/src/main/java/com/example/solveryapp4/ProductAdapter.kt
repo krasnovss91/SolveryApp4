a389f16b776c844fbed8d9b2fa94a6b70dda6277
krasnovss91 <krasnovstanislav91@gmail.com>
2022-02-14 15:16:21 +0400

    adapter_4

--- app/src/main/java/com/example/solveryapp4/ProductAdapter.kt
+++ app/src/main/java/com/example/solveryapp4/ProductAdapter.kt
@@ -1,19 +1,23 @@
 package com.example.solveryapp4
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 
-class ProductAdapter(private val products: List<Product>): RecyclerView.Adapter<ProductAdapter.MyViewHolder> {
+class ProductAdapter(private val products: List<Product>) :
+    RecyclerView.Adapter<ProductAdapter.MyViewHolder>() {
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val largeTextView: TextView = itemView.findViewById(R.id.textViewLarge)
         val smallTextView: TextView = itemView.findViewById(R.id.textViewSmall)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("Not yet implemented")
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.listview_item, parent, false)
+        return MyViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
@@ -25,12 +29,6 @@ class ProductAdapter(private val products: List<Product>): RecyclerView.Adapter<
 }
 /*
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.recyclerview_item, parent, false)
-        return MyViewHolder(itemView)
-    }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.largeTextView.text = names[position]
