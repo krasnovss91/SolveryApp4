8b89f921ec5ee5cef3703452135a836bf8c1d51c
krasnovss91 <krasnovstanislav91@gmail.com>
2022-02-14 14:30:04 +0400

    adapter_2

--- app/src/main/java/com/example/solveryapp4/ProductAdapter.kt
+++ app/src/main/java/com/example/solveryapp4/ProductAdapter.kt
@@ -1,4 +1,44 @@
 package com.example.solveryapp4
 
-class ProductAdapter {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class ProductAdapter(private val products: List<Product>): RecyclerView.Adapter<ProductAdapter.MyViewHolder> {
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val largeTextView: TextView = itemView.findViewById(R.id.textViewLarge)
+        val smallTextView: TextView = itemView.findViewById(R.id.textViewSmall)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
+/*
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recyclerview_item, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.largeTextView.text = names[position]
+        holder.smallTextView.text = "кот"
+    }
+
+    override fun getItemCount() = names.size
+}
+ */
