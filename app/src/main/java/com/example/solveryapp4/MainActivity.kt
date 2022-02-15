f7717b78ede4e73c844eb24f16b3b5e574eb8c84
krasnovss91 <krasnovstanislav91@gmail.com>
2022-02-15 11:17:26 +0400

    activity_12

--- app/src/main/java/com/example/solveryapp4/MainActivity.kt
+++ app/src/main/java/com/example/solveryapp4/MainActivity.kt
@@ -2,12 +2,21 @@ package com.example.solveryapp4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //MyApplication1425
+
+        val recyclerView: RecyclerView = findViewById(R.id.productList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = ProductAdapter()//сюда список товаров
+
+
     }
 }
