4905848a094dffd4a08892524714fb8c9bbcec6a
krasnovss91 <krasnovstanislav91@gmail.com>
2022-02-17 14:40:37 +0400

    addProduct_5

--- app/src/main/java/com/example/solveryapp4/ProductActivity.kt
+++ app/src/main/java/com/example/solveryapp4/ProductActivity.kt
@@ -2,10 +2,18 @@ package com.example.solveryapp4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class ProductActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product2)
+
+        val name = findViewById<View>(R.id.nameEdit)
+        val producer = findViewById<View>(R.id.producerEdit)
+        val cost = findViewById<View>(R.id.costEdit)
+
+        val saveProduct = findViewById<Button>(R.id.saveProduct)
     }
 }
