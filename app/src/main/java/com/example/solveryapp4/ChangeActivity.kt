21f4cd1cf1ed4e53345174a028468098c657348e
krasnovss91 <krasnovstanislav91@gmail.com>
2022-02-18 17:05:47 +0400

    editActivity_5

--- app/src/main/java/com/example/solveryapp4/ChangeActivity.kt
+++ app/src/main/java/com/example/solveryapp4/ChangeActivity.kt
@@ -1,5 +1,6 @@
 package com.example.solveryapp4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -15,6 +16,8 @@ class ChangeActivity : AppCompatActivity() {
         val editButton = findViewById<Button>(R.id.editButton)
 
         //поймать экземпляр product и передать дальше
+        val product = intent.getParcelableExtra<Product>(PRODUCT)
+        val editIntent = Intent(this, EditActivity::class.java)
 
         deleteButton.setOnClickListener(object : View.OnClickListener {//удалить
             override fun onClick(p0: View?) {
@@ -24,6 +27,7 @@ class ChangeActivity : AppCompatActivity() {
 
         editButton.setOnClickListener(object : View.OnClickListener{//редактировать
             override fun onClick(p0: View?) {
+
 
             }
         })
