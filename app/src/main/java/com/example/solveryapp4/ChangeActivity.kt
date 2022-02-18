c4faffae93d49dd7af8401c3d6d97929f1bce4c9
krasnovss91 <krasnovstanislav91@gmail.com>
2022-02-18 13:14:01 +0400

    changeActivity_2

--- app/src/main/java/com/example/solveryapp4/ChangeActivity.kt
+++ app/src/main/java/com/example/solveryapp4/ChangeActivity.kt
@@ -2,10 +2,28 @@ package com.example.solveryapp4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class ChangeActivity : AppCompatActivity() {//сюда 2 кнопки, что сделать с текущим элементом
+class ChangeActivity : AppCompatActivity() {
+    //сюда 2 кнопки, что сделать с текущим элементом
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change)
+
+        val deleteButton = findViewById<Button>(R.id.deleteButton)
+        val editButton = findViewById<Button>(R.id.editButton)
+
+        deleteButton.setOnClickListener(object : View.OnClickListener {//удалить
+            override fun onClick(p0: View?) {
+
+            }
+        })
+
+        editButton.setOnClickListener(object : View.OnClickListener{//редактировать
+            override fun onClick(p0: View?) {
+
+            }
+        })
     }
 }
