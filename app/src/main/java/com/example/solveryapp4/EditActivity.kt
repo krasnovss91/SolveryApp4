714d2e1a365070b827fea43ebad0911d18824ee0
krasnovss91 <krasnovstanislav91@gmail.com>
2022-02-18 16:03:56 +0400

    editActivity_2

--- app/src/main/java/com/example/solveryapp4/EditActivity.kt
+++ app/src/main/java/com/example/solveryapp4/EditActivity.kt
@@ -2,10 +2,17 @@ package com.example.solveryapp4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class EditActivity : AppCompatActivity() {//здесь редактирование элемента списка
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+
+    val name = findViewById<EditText>(R.id.nameEdit1)
+    val producer = findViewById<EditText>(R.id.producerEdit1)
+    val cost = findViewById<EditText>(R.id.costEdit1)
+
+
     }
 }
