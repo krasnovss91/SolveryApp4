077a92f2b4771fbcec6a2dbbb444ddaeeed966ea
krasnovss91 <krasnovstanislav91@gmail.com>
2022-02-20 18:54:50 +0400

    editItem_1

--- app/src/main/java/com/example/solveryapp4/EditActivity.kt
+++ app/src/main/java/com/example/solveryapp4/EditActivity.kt
@@ -6,25 +6,31 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 
-class EditActivity : AppCompatActivity() {//здесь редактирование элемента списка
+class EditActivity : AppCompatActivity() {
+    //здесь редактирование элемента списка
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
 
-    val name = findViewById<EditText>(R.id.nameEdit1)
-    val producer = findViewById<EditText>(R.id.producerEdit1)
-    val cost = findViewById<EditText>(R.id.costEdit1)
+        val name = findViewById<EditText>(R.id.nameEdit1)
+        val producer = findViewById<EditText>(R.id.producerEdit1)
+        val cost = findViewById<EditText>(R.id.costEdit1)
 
-    val editButton = findViewById<Button>(R.id.saveProduct1)
-    val product = intent.getParcelableExtra<Product>(PRODUCT)//разложить по EditText для редактирования
+        val editButton = findViewById<Button>(R.id.saveProduct1)
+        val product =
+            intent.getParcelableExtra<Product>(PRODUCT)
 
-        //  name.text = product.name
 
-    editButton.setOnClickListener(object : View.OnClickListener{//вернуть отредактированный экземпляр в главную activity
-        override fun onClick(p0: View?) {
+        name.setText(product?.name)
+        producer.setText(product?.producer)
+        cost.setText(product?.cost.toString())
 
-        }
-    })
+        editButton.setOnClickListener(object : View.OnClickListener {
+            //вернуть отредактированный экземпляр в главную activity
+            override fun onClick(p0: View?) {
+
+            }
+        })
 
     }
 }
