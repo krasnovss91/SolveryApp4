ad4f3aa8b056652d4411e88117b5f34e0faf7a92
krasnovss91 <krasnovstanislav91@gmail.com>
2022-02-21 10:54:24 +0400

    editItem_2

--- app/src/main/java/com/example/solveryapp4/EditActivity.kt
+++ app/src/main/java/com/example/solveryapp4/EditActivity.kt
@@ -25,10 +25,12 @@ class EditActivity : AppCompatActivity() {
         producer.setText(product?.producer)
         cost.setText(product?.cost.toString())
 
-        editButton.setOnClickListener(object : View.OnClickListener {
-            //вернуть отредактированный экземпляр в главную activity
-            override fun onClick(p0: View?) {
+        val image = product?.avatar
 
+        editButton.setOnClickListener(object : View.OnClickListener {
+            //вернуть отредактированный экземпляр в главную activity, собрать новый экземпляр. Картинку взять из старого
+            override fun onClick(p0: View?) {
+               val result = image?.let { Product(it,name.text.toString(),producer.text.toString(),cost.text.toString().toInt()) }
             }
         })
 
